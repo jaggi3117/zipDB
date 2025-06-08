c42215349e5aaa8cdd7eb002fb5accc36c51fac0
jagmohan sharma <jagmohan.20223117@gmail.com>
2025-06-08 12:34:01 +0000

    minor fixes and README.md update

--- src/RedisServer.cpp
+++ src/RedisServer.cpp
@@ -23,6 +23,9 @@ void signalHandler(int signum){
     exit(signum);
 }
 
+void RedisServer::setupSignalHandler() {
+    signal(SIGINT, signalHandler);
+}
 RedisServer::RedisServer(int port) : port(port), server_socket(-1), running(true){
     globalServer = this;
     setupSignalHandler();
@@ -78,16 +81,36 @@ void RedisServer::run(){
     std::vector<std::thread> threads;
     RedisCommandHandler cmdHandler; //implement later 
 
-    while(running){
+    while (running) {
         int client_socket = accept(server_socket, nullptr, nullptr);
-        if(client_socket < 0){
-            if(running){
-                std::cerr<<"error accepting client connection \n";
-                break;
-            }
+        if (client_socket < 0) {
+            if (running) 
+                std::cerr << "Error Accepting Client Connection\n";
+            break;
         }
 
-        
+        //setup a beautiful thread lol for this shit
+        threads.emplace_back([client_socket, &cmdHandler](){
+            char buffer[1024]; //buffer to recv client msg
+            while (true) {
+                memset(buffer, 0, sizeof(buffer)); //set buffer memory block to 0
+                int bytes = recv(client_socket, buffer, sizeof(buffer) - 1, 0); //recv sys call
+                if (bytes <= 0) break; //if recv return bytes count <= 0
+                std::string request(buffer, bytes); //might read garbage in string request thats why bytes must be specified
+                std::string response = cmdHandler.processCommand(request);
+                send(client_socket, response.c_str(), response.size(), 0);
+            }
+            close(client_socket);
+        });
+    }
+    
+    for (auto& t : threads) {
+        if (t.joinable()) t.join();
     }
 
+    // Before shutdown, persist the database
+    if (RedisDatabase::getInstance().dump("dump.my_rdb"))
+        std::cout << "Database Dumped to dump.my_rdb\n";
+    else 
+        std::cerr << "Error dumping database\n";
 }
